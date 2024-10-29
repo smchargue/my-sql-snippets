264e2f23cf16ef964bfd320bdd9f224bd7223306
Steve McHargue <smchargue@outlook.com>
2024-10-29 06:50:25 -0500

    Update GetNextBirthday.sql

--- GetNextBirthday.sql
+++ GetNextBirthday.sql
@@ -1,24 +1,42 @@
-CREATE FUNCTION dbo.GetNextBirthday(@Month INT, @Day INT)
-RETURNS DATE
-AS
+CREATE FUNCTION [dbo].[TryDate]
+(
+	@year as INT,
+	@month as INT,
+	@day as INT
+)
+RETURNS DATE AS 
 BEGIN
-    DECLARE @Today DATE = GETDATE();
-    DECLARE @Year INT = YEAR(@Today);
-    DECLARE @NextBirthday DATE;
+	RETURN TRY_CONVERT(DATE,CONCAT(FORMAT(@year,'D4'), FORMAT(@month,'D2'), FORMAT(@day,'D2')))
+END
+GO
 
-    IF (@Month = 2 AND @Day = 29)
-    BEGIN
-        IF (DATEFROMPARTS(@Year, 2, 28) >= @Today OR @Year % 4 = 0 AND @Year % 100 != 0 OR @Year % 400 = 0)
-            SET @NextBirthday = DATEFROMPARTS(@Year, 2, 29);
-        ELSE
-            SET @NextBirthday = DATEFROMPARTS(@Year + 1, 2, 29);
-    END
-    ELSE
-    BEGIN
-        SET @NextBirthday = DATEFROMPARTS(@Year, @Month, @Day);
-        IF @NextBirthday < @Today
-            SET @NextBirthday = DATEFROMPARTS(@Year + 1, @Month, @Day);
-    END
+CREATE FUNCTION [dbo].[GetNextBirtyday]
+(
+	@month as INT,
+	@day as INT
+) 
+RETURNS DATE AS
+BEGIN
 
-    RETURN @NextBirthday;
+	DECLARE @today DATE = GetDATE(); 
+    DECLARE @year INT = YEAR(@today);
+    DECLARE @returnDate DATE = NULL;
+
+	IF ISNULL(@month,0) BETWEEN 1 AND 12 AND ISNULL(@day,0) BETWEEN 1 AND 31
+	BEGIN
+		IF (@month = 2 AND @day = 29)
+		BEGIN 
+			SET @returnDate = ISNULL(dbo.TryDate(@year,@month,@day), DATEFROMPARTS(@year,@month,28))
+			IF @returnDate < @today 
+				SET @returnDate = ISNULL(dbo.TryDate(@year+1,@month,@day), DATEFROMPARTS(@year+1,@month,28))
+			RETURN @returnDate
+		END
+
+		SET @returnDate = DATEFROMPARTS(@year, @month, @day) 
+
+		IF @returnDate < @today 
+			SET @returnDate = DATEFROMPARTS(@year+1, @month, @day) 		
+	END
+
+	RETURN @returnDate
 END
